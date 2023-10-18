1f3e939c9e6d9049e72885e7f48ffc6735b0c148
Marcelo <marceloreijunho@gmail.com>
2023-10-18 00:55:46 -0300

    first commit

---
+++ src/main/java/com/marcelofidelis/mercedes_project/MercedesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.marcelofidelis.mercedes_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercedesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercedesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelofidelis/mercedes_project/MercedesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelofidelis.mercedes_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercedesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

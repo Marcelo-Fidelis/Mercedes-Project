5b3ddd5b68f4e551448d493b19aacc87a8d65acd
Marcelo <marceloreijunho@gmail.com>
2023-10-21 20:01:42 -0300

    refactor: Change the class' name for convention

---
+++ src/test/java/com/marcelofidelis/mercedes_project/servicestests/TicketServiceImplTest.java
@@ -0,0 +1,9 @@
+package com.marcelofidelis.mercedes_project.servicestests;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class TicketServiceImplTest {
+    
+}

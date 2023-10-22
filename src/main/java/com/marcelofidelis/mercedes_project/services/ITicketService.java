6a6b0a8b3762dc1cba8481c45820e056ba26669c
Marcelo <marceloreijunho@gmail.com>
2023-10-22 15:13:30 -0300

    feat: Addition of ITicketService.java and TicketServiceImpl.java classes

---
+++ src/main/java/com/marcelofidelis/mercedes_project/services/ITicketService.java
@@ -0,0 +1,22 @@
+package com.marcelofidelis.mercedes_project.services;
+
+
+
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.marcelofidelis.mercedes_project.domain.Ticket;
+
+
+
+@Service
+public interface ITicketService {
+    public Ticket getTicketByTitle(String title) throws Exception;
+
+    public void updateTicketAttendent(int attendant);
+
+    public Ticket getTicketById(UUID uuid) throws Exception;     
+   
+}
---
+++ src/main/java/com/marcelofidelis/mercedes_project/services/TicketServiceImpl.java
@@ -0,0 +1,34 @@
+package com.marcelofidelis.mercedes_project.services;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelofidelis.mercedes_project.domain.Ticket;
+import com.marcelofidelis.mercedes_project.repositories.TicketRepository;
+
+@Service
+public class TicketServiceImpl implements ITicketService {
+
+    @Autowired
+    TicketRepository ticketRepository;
+
+    @Override
+    public Ticket getTicketByTitle(String title) throws Exception {
+        Ticket found = ticketRepository.findByTitle(title).orElseThrow(() -> new Exception("Erro"));
+        return found;
+    }
+
+    @Override
+    public void updateTicketAttendent(int attendant) {
+        
+    }
+
+    @Override
+    public Ticket getTicketById(UUID uuid) throws Exception {
+        Ticket found = ticketRepository.findById(uuid).orElseThrow(() -> new Exception("Erro"));
+        return found;
+    }
+}

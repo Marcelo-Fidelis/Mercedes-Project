f366b228a9337b27ec705102bb33967c04ec3904
Marcelo <marceloreijunho@gmail.com>
2023-11-06 09:43:40 -0300

    fix: Column annotation and Hashcode added to the Department Entity.

--- src/main/java/com/marcelofidelis/mercedes_project/domain/Department.java
+++ src/main/java/com/marcelofidelis/mercedes_project/domain/Department.java
@@ -1,27 +1,33 @@
 package com.marcelofidelis.mercedes_project.domain;
 
+import java.io.Serializable;
+
 import com.marcelofidelis.mercedes_project.domain.dtos.DtoDepartment;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Table(name ="department")
-@Entity
+@Entity(name ="department")
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    
+    @Column
     private String title;
+    @Column
     private String description;
 
     public Department(DtoDepartment dto){
